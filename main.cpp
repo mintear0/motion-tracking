d3371c87d9d4d99b4a3881a3eb197e526fc886c1
mintear0 <yuuki10020@gmail.com>
2020-03-12 22:46:30 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "opencv2/opencv.hpp"
+
+int main()
+{
+	cv::namedWindow("miku_movie", cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture cap;
+	cap.open("D:/videos/Desktop 2019.07.19 - 17.53.14.01.mp4");
+	if (!cap.isOpened()) {
+		return -1;
+	}
+
+	double fps = cap.get(cv::CAP_PROP_FPS);
+
+	cv::Mat frame;
+	for (;;) {
+		cap >> frame;
+		if (frame.empty()) break;
+		cv::imshow("miku_movie", frame);
+		if ((char)cv::waitKey((int)1000 / fps) >= 0) break;
+	}
+}
